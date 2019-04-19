bf52b038eff33de04fe0b85f8ccfa459ecd43661
Tammy Metz <tammy@tammymetz.com>
2019-04-19 02:03:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function driversWithRevenueOver(drivers, revenue) {
 
 function driverNamesWithRevenueOver(drivers, revenue) {
   const richDrivers = drivers.filter(function(driver) { return driver.revenue > revenue});
-  return map(richDrivers, function(driver) {
+  const driverNames = richDrivers.map(function(driver) {
     return driver.name;
-  })
+  });
+  return richDrivers;
 }
